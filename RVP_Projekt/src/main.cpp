a4542a59372110753b5732cb19622e55ea80df64
Raimo Köidam <raimokoidam@gmail.com>
2024-01-10 18:37:30 +0200

    LED riba kood

--- RVP_Projekt/src/main.cpp
+++ RVP_Projekt/src/main.cpp
@@ -1,29 +1,38 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 30
+#define LED_DATA_PIN 15
+
+// Led riba
+CRGB leds[NUM_LEDS];
+
+// LCD
 LiquidCrystal lcd(21, 2, 19, 18, 5, 17, 16);
+
+// Mootorid
 byte directionPin = 33;
 byte stepPin = 32;
 int numberOfSteps = 400;
 int nsleep = 23;
 
-
-
 // put function declarations here:
-int myFunction(int, int);
-void moveStepper(int, int ,int);
+
+void moveStepper(int, int, int);
 
 void setup()
 {
-  // put your setup code here, to run once:
   pinMode(22, OUTPUT);
   pinMode(directionPin, OUTPUT);
   pinMode(stepPin, OUTPUT);
   pinMode(nsleep, OUTPUT);
-  digitalWrite(nsleep, HIGH);  // Nsleep High
+  digitalWrite(nsleep, HIGH); // Nsleep High
 
-  // pinMode(2, OUTPUT);
-  // digitalWrite(2, LOW);
-  //  put your setup code here, to run once: RW2
+  // LED riba
+  FastLED.addLeds<WS2813, LED_DATA_PIN, RGB>(leds, NUM_LEDS);
+
+  // LCD
   lcd.begin(16, 2);
   lcd.clear();
   lcd.print("LCD");
@@ -37,23 +46,26 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  digitalWrite(22, HIGH);
-  delay(1000);
-  digitalWrite(22, LOW);
-  delay(1000);
-
   // Motor control
-  digitalWrite(directionPin, HIGH); 
+  digitalWrite(directionPin, HIGH);
   moveStepper(numberOfSteps, 50, 2);
-  digitalWrite(directionPin, LOW); 
+  digitalWrite(directionPin, LOW);
   moveStepper(numberOfSteps, 50, 2);
 
+  // LED riba
+  leds[0] = CRGB::Blue;
+  leds[1] = CRGB::Green;
+  leds[2] = CRGB::Red;
+  leds[3] = CRGB::Yellow;
+  FastLED.show();
 
+  delay(500);
 }
 
-void moveStepper(int steps, int pulseWidthMicros, int millisBetweenSteps) {
-  for (int n = 0; n < steps; n++) {
+void moveStepper(int steps, int pulseWidthMicros, int millisBetweenSteps)
+{
+  for (int n = 0; n < steps; n++)
+  {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(pulseWidthMicros);
     digitalWrite(stepPin, LOW);
