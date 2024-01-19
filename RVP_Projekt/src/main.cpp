8b7ebf46f1557652c5fde27124abe574153cd8ce
Kaur <kaur.kullamae@ut.ee>
2024-01-19 10:45:44 +0200

    Wifi handling?

--- RVP_Projekt/src/main.cpp
+++ RVP_Projekt/src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <FastLED.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <time.h>
+
+
 
 #define NUM_LEDS 30
 #define LED_DATA_PIN 15
@@ -17,12 +22,24 @@ byte stepPin = 32;
 int numberOfSteps = 400;
 int nsleep = 23;
 
+// Variables
+
+const char *ssid = "Hotspot";
+const char *password = "123456789";
+byte LEDH = 13;
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 3600;
+
+AsyncWebServer server(80);
 // put function declarations here:
 
 void moveStepper(int, int, int);
 
 void setup()
 {
+  Serial.begin(115200);
+  pinMode(LEDH,OUTPUT);
   pinMode(22, OUTPUT);
   pinMode(directionPin, OUTPUT);
   pinMode(stepPin, OUTPUT);
@@ -42,11 +59,61 @@ void setup()
   lcd.print((char)153);
   lcd.print((char)153);
   lcd.print("tab");
+
+  // Hotspot setup
+
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Hotspot IP address: ");
+  Serial.println(IP);
+
+  // HTML leht
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", "<html><body>\
+      <form action='/connect' method='POST'>\
+        SSID: <input type='text' name='ssid'><br>\
+        Password: <input type='password' name='password'><br>\
+        <input type='submit' value='Connect'>\
+      </form>\
+    </body></html>");
+  });
+
+  // WIFI data from user
+  server.on("/connect", HTTP_POST, [](AsyncWebServerRequest *request){
+    String ssid, password;
+    if(request->hasParam("ssid", true) && request->hasParam("password", true)){
+      ssid = request->getParam("ssid", true)->value();
+      password = request->getParam("password", true)->value();
+
+      // Connect to the specified WiFi network
+      WiFi.begin(ssid.c_str(), password.c_str());
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+      }
+
+      // Initialize and synchronize time
+      configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+      while (time(nullptr) <= 0) {
+        delay(1000);
+        Serial.println("Waiting for time synchronization...");
+      }
+
+      request->redirect("/");
+    }
+    else {
+      request->send(400, "text/plain", "Bad Request");
+    }
+  });
+
+  // Start server 
+  server.begin();
 }
 
 void loop()
 {
   // Motor control
+  /*
   digitalWrite(directionPin, HIGH);
   moveStepper(numberOfSteps, 50, 2);
   digitalWrite(directionPin, LOW);
@@ -58,7 +125,19 @@ void loop()
   leds[2] = CRGB::Red;
   leds[3] = CRGB::Yellow;
   FastLED.show();
+  */
+ Serial.println("Tere");
+  if (Serial.available() > 0) {
+          String input = Serial.readStringUntil('\n');
+          if (input == "on") {
+          digitalWrite(LEDH, HIGH);
+          Serial.println("On");
+          } else if (input == "off") {
+          digitalWrite(LEDH, LOW);
+          Serial.println("Off");
 
+          } 
+      }
   delay(500);
 }
 
